1d22a97326ca9fd30466fd6f5f21df050177c232
Arya Zand <28754724+aryazand@users.noreply.github.com>
2023-02-26 20:29:03 -0600

    document sample data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Sample PRO-seq Reads
+#'
+#' Random sample of PRO-seq reads aligning to CMV genome. Experiment was conducted
+#' in D-NT2 cells infected with CMV
+#'
+#' @format ## `sample_NT2_data`
+#' A GRanges object with 2184 reads and 2 metadata columns:
+#' \describe{
+#'   \item{name}{name assigned to read by sequencer}
+#'   \item{score}{number of reads aligning to the range}
+#' }
+#' @source "In house experiment"
+"sample_NT2_data"
